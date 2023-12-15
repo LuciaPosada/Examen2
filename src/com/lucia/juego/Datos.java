6d202c0705fb8f310a3d77aa84030d0b1161f5f0
Lucia <lposadagonzalez@danielcastelao.org>
2023-12-15 09:54:39 +0100

    cambios en el programa

--- src/com/lucia/juego/Datos.java
+++ src/com/lucia/juego/Datos.java
@@ -1,98 +1,88 @@
 package com.lucia.juego;
 
 public class Datos {
-
-        /**
-         * numero generado para adivinar
-         */
-        private int numero;
-        /**
-         * numero introduccido por el usuario
-         */
-        private int numeroAdivinado;
-        /**
-         * numero de intentos maximos
-         */
-        private int intentosMax;
-        /**
-         * contador de los intentos restantes
-         */
-        private int contadorIntentos;
-        /**
-         * numero de aciertos por partida
-         */
-        private int aciertos;
-        /**
-         * alias del jugador
-         */
-        private String alias;
-        /**
-         * numero de fallos
-         */
-        private int fallos;
-        /**
-         * numero de rondas maximas
-         */
-        private int rondasPartida;
-        /**
-         * ronda actual
-         */
-        private int rondaActual;
     /**
-     * puntuacion del record
+     * numero generado para adivinar
      */
-    private int puntuacionRecord;
+    private int numero;
     /**
-     * alias del record
+     * numero introduccido por el usuario
      */
-    private String aliasRecord;
+    private int numeroAdivinado;
+    /**
+     * numero de intentos maximos
+     */
+    private int intentosMax;
+    /**
+     * contador de los intentos restantes
+     */
+    private int contadorIntentos;
+    /**
+     * numero de aciertos por partida
+     */
+    private int aciertos;
+    /**
+     * alias del jugador
+     */
+    private String alias;
+    /**
+     * numero de fallos
+     */
+    private int fallos;
 
+    private static Record record;
 
-        static Datos dato = new Datos();
-        public static Datos getInstance(){
-            return dato;
-        }
-        public void setNumero(int numero) {
-            this.numero = numero;
-        }
-        public void setIntentosMax(int intentosMax) {
-            this.intentosMax = intentosMax;
-        }
-        public void setNumeroAdivinado(int numeroAdivinado) {
-            this.numeroAdivinado = numeroAdivinado;
-        }
-        public void setContadorIntentos(int contadorIntentos) {
-            this.contadorIntentos = contadorIntentos;
-        }
-        public void setAciertos(int aciertos) {
-            this.aciertos = aciertos;
-        }
-        public void setAlias(String alias) {
-            this.alias = alias;
-        }
-        public void setFallos(int fallos) {
-            this.fallos = fallos;
-        }
+    private static Partida ronda;
 
-        public int getNumero() {
-            return numero;
-        }
-        public int getNumeroAdivinado() {
-            return numeroAdivinado;
-        }
-        public int getIntentosMax() {
-            return intentosMax;
-        }
-        public int getContadorIntentos() {
-            return contadorIntentos;
-        }
-        public int getAciertos() {
-            return aciertos;
-        }
-        public String getAlias() {
-            return alias;
-        }
-        public int getFallos() {
-            return fallos;
-        }
+    private Datos() {
+
+    }
+
+    static Datos dato = new Datos();
+    public static Datos getInstance(){
+        return dato;
+    }
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    public void setIntentosMax(int intentosMax) {
+        this.intentosMax = intentosMax;
+    }
+    public void setNumeroAdivinado(int numeroAdivinado) {
+        this.numeroAdivinado = numeroAdivinado;
+    }
+    public void setContadorIntentos(int contadorIntentos) {
+        this.contadorIntentos = contadorIntentos;
+    }
+    public void setAciertos(int aciertos) {
+        this.aciertos = aciertos;
+    }
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+    public void setFallos(int fallos) {
+        this.fallos = fallos;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+    public int getNumeroAdivinado() {
+        return numeroAdivinado;
+    }
+    public int getIntentosMax() {
+        return intentosMax;
+    }
+    public int getContadorIntentos() {
+        return contadorIntentos;
+    }
+    public int getAciertos() {
+        return aciertos;
+    }
+    public String getAlias() {
+        return alias;
+    }
+    public int getFallos() {
+        return fallos;
+    }
 }
